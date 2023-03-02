152d2994fa4e3b04ce6198d8841426f6dff9d80e
Giorgio Resci <giorgio.resci@xnumeris.com>
2023-03-02 09:42:53 +0100

    fix asgi

--- backend/djangoapp/asgi.py
+++ backend/djangoapp/asgi.py
@@ -7,11 +7,12 @@ import os
 import dotenv
 import django
 from channels.routing import get_default_application
-from djangoapp.routing  import application as app
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.environ.get("DJANGO_SETTINGS_MODULE", "djangoapp.settings"))
 django.setup()
+from djangoapp.routing  import application as app
 
 #application = routing.application
 application = app
